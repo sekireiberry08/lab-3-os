bb6aaf9ca80ed6cc5b456788d5a34bc276ddd054
BlueDekiVN <an.trinhrankdong@hcmut.edu.vn>
2026-06-01 22:58:58 +0700

    done part 2

--- p2pc/pc.c
+++ p2pc/pc.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <semaphore.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #define BUF_SIZE 2
 #define THREADS 1 // 1 producer and 1 consumer
@@ -13,6 +14,9 @@ int fill = 0;
 int use = 0;
 
 /*TODO: Fill in the synchronization stuff */
+pthread_mutex_t mutex;
+sem_t empty;
+sem_t full;
 
 void put(int value); // put data into buffer
 int get();           // get data from buffer
@@ -22,11 +26,20 @@ void * producer(void * arg) {
   int tid = *((int*) arg);
   for (i = 0; i < LOOPS; i++) {
     /*TODO: Fill in the synchronization stuff */
+    sem_wait(&empty); // đợi có chỗ trống
+    pthread_mutex_lock(&mutex);
     put(i); // line P2
+    pthread_mutex_unlock(&mutex);
+    sem_post(&full); // báo có thêm 1 item
     printf("Producer %d put data %d\n", tid, i);
     sleep(1);
     /*TODO: Fill in the synchronization stuff */
   }
+  sem_wait(&empty);
+  pthread_mutex_lock(&mutex);
+  put(-1);
+  pthread_mutex_unlock(&mutex);
+  sem_post(&full);
   pthread_exit(NULL);
 }
 
@@ -35,7 +48,11 @@ void * consumer(void * arg) {
   int tid = *((int*) arg);
   while (tmp != -1) {
     /*TODO: Fill in the synchronization stuff */
+    sem_wait(&full); // đợi có item
+    pthread_mutex_lock(&mutex);
     tmp = get(); // line C2
+    pthread_mutex_unlock(&mutex);
+    sem_post(&empty); // trả slot trống
     printf("Consumer %d get data %d\n", tid, tmp);
     sleep(1);
     /*TODO: Fill in the synchronization stuff */
@@ -50,6 +67,9 @@ int main(int argc, char ** argv) {
   pthread_t consumers[THREADS];
 
   /*TODO: Fill in the synchronization stuff */
+  pthread_mutex_init(&mutex, NULL);
+  sem_init(&empty, 0, BUF_SIZE);
+  sem_init(&full, 0, 0);
 
   for (i = 0; i < THREADS; i++) {
     tid[i] = i;
@@ -66,7 +86,9 @@ int main(int argc, char ** argv) {
   }
 
   /*TODO: Fill in the synchronization stuff destroy (if needed) */
-
+  pthread_mutex_destroy(&mutex);
+  sem_destroy(&empty);
+  sem_destroy(&full);
   return 0;
 }
 
